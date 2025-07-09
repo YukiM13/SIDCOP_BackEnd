cca1a6a76ff0be4f1e540518171e57c3ed4c5c47
AlexxxMikuUwU <xdavid927@gmail.com>
2025-07-09 13:45:39 -0600

    feat(Estados Civiles Listar)

---
+++ SIDCOP/SIDCOP_Backend.DataAccess/Repositories/General/EstadoCivilRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using SIDCOP_Backend.Entities.Entities;
+
+namespace SIDCOP_Backend.DataAccess.Repositories.General
+{
+    public class EstadoCivilRepository : IRepository<tbEstadosCiviles>
+    {
+        public RequestStatus Delete(tbEstadosCiviles item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public tbEstadosCiviles Find(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RequestStatus Insert(tbEstadosCiviles item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<tbEstadosCiviles> List()
+        {
+            var parameter = new DynamicParameters();
+
+
+            using var db = new SqlConnection(SIDCOP_Context.ConnectionString);
+            var result = db.Query<tbEstadosCiviles>(ScriptDatabase.EstadosCiviles_Listar, parameter, commandType: System.Data.CommandType.StoredProcedure).ToList();
+
+
+            return result;
+        }
+
+        public RequestStatus Update(tbEstadosCiviles item)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
