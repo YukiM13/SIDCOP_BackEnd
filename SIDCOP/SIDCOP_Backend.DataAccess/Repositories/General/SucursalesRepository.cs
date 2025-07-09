4de63956d4ee8b79e798e2a93cb619b6c7ae5915
Samuel <107447969+chavez-113@users.noreply.github.com>
2025-07-09 13:48:54 -0600

    Avances
    Script de:
    -Sucursales
    -Productos
    
    repository de sucursales con el insertar

---
+++ SIDCOP/SIDCOP_Backend.DataAccess/Repositories/General/SucursalesRepository.cs
@@ -0,0 +1,66 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using SIDCOP_Backend.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIDCOP_Backend.DataAccess.Repositories.General
+{
+    class SucursalesRepository : IRepository<tbSucursales>
+    {
+        public RequestStatus Delete(tbSucursales item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public tbSucursales Find(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RequestStatus Insert(tbSucursales item)
+        {
+            if (item == null)
+            {
+                return new RequestStatus { CodeStatus = 0, MessageStatus = "Los datos llegaron vacios o datos erroneos" };
+            }
+            var parameter = new DynamicParameters();
+            parameter.Add("@Sucu_Descripcion", item.Sucu_Descripcion, System.Data.DbType.String, System.Data.ParameterDirection.Input);
+            parameter.Add("@Colo_Id", item.Colo_Id, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
+            parameter.Add("@Sucu_DireccionExacta", item.Sucu_DireccionExacta, System.Data.DbType.String, System.Data.ParameterDirection.Input);
+            parameter.Add("@Sucu_Telefono1", item.Sucu_Telefono1, System.Data.DbType.String, System.Data.ParameterDirection.Input);
+            parameter.Add("@Sucu_Telefono2", item.Sucu_Telefono2, System.Data.DbType.String, System.Data.ParameterDirection.Input);
+            parameter.Add("@Sucu_Correo", item.Sucu_Correo, System.Data.DbType.String, System.Data.ParameterDirection.Input);
+            parameter.Add("@Usua_Creacion", item.Usua_Creacion, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
+            parameter.Add("@Sucu_FechaCreacion", item.Sucu_FechaCreacion, System.Data.DbType.DateTime, System.Data.ParameterDirection.Input);
+
+            try
+            {
+                using var db = new SqlConnection(SIDCOP_Context.ConnectionString);
+                var result = db.QueryFirstOrDefault<RequestStatus>(ScriptDatabase.Sucursal_Insertar, parameter, commandType: System.Data.CommandType.StoredProcedure);
+                if (result == null)
+                {
+                    return new RequestStatus { CodeStatus = 0, MessageStatus = "Error desconocido" };
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return new RequestStatus { CodeStatus = 0, MessageStatus = $"Error inesperado: {ex.Message}" };
+            }
+        }
+
+        public IEnumerable<tbSucursales> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public RequestStatus Update(tbSucursales item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SIDCOP/SIDCOP_Backend.DataAccess/ScriptDatabase.cs
+++ SIDCOP/SIDCOP_Backend.DataAccess/ScriptDatabase.cs
@@ -12,5 +12,33 @@ namespace SIDCOP_Backend.DataAccess
         public const string Usuarios_Listar = "Acce.SP_Usuarios_Listar";
 
         #endregion
+
+        #region Productos
+
+        public const string Productos_Listar = "Inve.SP_Productos_Listar";
+
+        public const string Producto_Insertar = "Inve.SP_Producto_Insertar";
+
+        public const string Producto_Actualizar = "Inve.SP_Producto_Actualizar";
+
+        public const string Producto_Eliminar = "Inve.SP_Producto_Eliminar";
+
+        public const string Producto_Buscar = "Inve.SP_Producto_Buscar";
+
+        #endregion
+
+        #region Sucursales
+
+        public const string Sucursales_Listar = "Gral.SP_Sucursales_Listar";
+
+        public const string Sucursal_Insertar = "Gral.SP_Sucursal_Insertar";
+
+        public const string Sucursal_Actualizar = "Gral.SP_Sucursal_Actualizar";
+
+        public const string Sucursal_Eliminar = "Gral.SP_Sucursal_Eliminar";
+
+        public const string Sucursal_Buscar = "Gral.SP_Sucursal_Buscar";
+
+        #endregion
     }
 }
