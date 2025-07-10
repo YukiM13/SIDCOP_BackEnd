bffaea84635725326c75a9526907bfdfc4291dd4
JosueMencia <mjosuedavid12@gmail.com>
2025-07-10 14:44:53 -0600

    fix IRepository de Usuarios

--- SIDCOP/SIDCOP_Backend.DataAccess/Repositories/Acceso/UsuarioRepository.cs
+++ SIDCOP/SIDCOP_Backend.DataAccess/Repositories/Acceso/UsuarioRepository.cs
@@ -11,10 +11,7 @@ namespace SIDCOP_Backend.DataAccess.Repositories.Acceso
 {
     public class UsuarioRepository : IRepository<tbUsuarios>
     {
-        public RequestStatus Delete(int? id)
-        {
-            throw new NotImplementedException();
-        }
+      
 
         public RequestStatus Delete(int? id)
         {
