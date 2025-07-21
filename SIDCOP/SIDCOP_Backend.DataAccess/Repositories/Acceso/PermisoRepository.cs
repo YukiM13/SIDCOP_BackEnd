ae21966bb43e444a421ddd3247515ffd813574f8
Helen Euceda <neuceda13@gmail.com>
2025-07-21 11:38:19 -0600

    Listo Usuarios

--- SIDCOP/SIDCOP_Backend.DataAccess/Repositories/Acceso/PermisoRepository.cs
+++ SIDCOP/SIDCOP_Backend.DataAccess/Repositories/Acceso/PermisoRepository.cs
@@ -24,22 +24,40 @@ namespace SIDCOP_Backend.DataAccess.Repositories.Acceso
         }
         public RequestStatus Insert(tbPermisos item)
         {
+            if (item == null)
+            {
+                return new RequestStatus { code_Status = 0, message_Status = "Los datos llegaron vacios o datos erroneos" };
+            }
+
             var parameter = new DynamicParameters();
             parameter.Add("@Role_Id", item.Role_Id, System.Data.DbType.String, System.Data.ParameterDirection.Input);
             parameter.Add("@AcPa_Id", item.AcPa_Id, System.Data.DbType.String, System.Data.ParameterDirection.Input);
             parameter.Add("@Usua_Creacion", item.Usua_Creacion, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
             parameter.Add("@Perm_FechaCreacion", item.Perm_FechaCreacion, System.Data.DbType.DateTime, System.Data.ParameterDirection.Input);
 
-            using var db = new SqlConnection(SIDCOP_Context.ConnectionString);
-            var result = db.Execute(ScriptDatabase.Permiso_Insertar, parameter, commandType: System.Data.CommandType.StoredProcedure);
+            try
+            {
+                using var db = new SqlConnection(SIDCOP_Context.ConnectionString);
+                var result = db.QueryFirstOrDefault<RequestStatus>(ScriptDatabase.Permiso_Insertar, parameter, commandType: System.Data.CommandType.StoredProcedure);
 
-            string mensaje = (result == 0) ? "Error al insertar" : "Insertado correctamente";
-
-            return new RequestStatus { code_Status = result, message_Status = mensaje };
+                if (result == null)
+                {
+                    return new RequestStatus { code_Status = 0, message_Status = "Error desconocido." };
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return new RequestStatus { code_Status = 0, message_Status = $"Error inesperado: {ex.Message}" };
+            }
         }
 
         public RequestStatus Update(tbPermisos item)
         {
+            if (item == null)
+            {
+                return new RequestStatus { code_Status = 0, message_Status = "Los datos llegaron vacios o datos erroneos" };
+            }
             var parameter = new DynamicParameters();
             parameter.Add("@Perm_Id", item.Perm_Id, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
             parameter.Add("@Role_Id", item.Role_Id, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
@@ -47,12 +65,21 @@ namespace SIDCOP_Backend.DataAccess.Repositories.Acceso
             parameter.Add("@Usua_Modificacion", item.Usua_Modificacion, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
             parameter.Add("@Perm_FechaModificacion", item.Perm_FechaModificacion, System.Data.DbType.DateTime, System.Data.ParameterDirection.Input);
 
-            using var db = new SqlConnection(SIDCOP_Context.ConnectionString);
-            var result = db.Execute(ScriptDatabase.Permiso_Actualizar, parameter, commandType: System.Data.CommandType.StoredProcedure);
+            try
+            {
+                using var db = new SqlConnection(SIDCOP_Context.ConnectionString);
+                var result = db.QueryFirstOrDefault<RequestStatus>(ScriptDatabase.Permiso_Actualizar, parameter, commandType: System.Data.CommandType.StoredProcedure);
 
-            string mensaje = (result == 0) ? "Error al actualizar" : "Actualizado correctamente";
-
-            return new RequestStatus { code_Status = result, message_Status = mensaje };
+                if (result == null)
+                {
+                    return new RequestStatus { code_Status = 0, message_Status = "Error desconocido." };
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return new RequestStatus { code_Status = 0, message_Status = $"Error inesperado: {ex.Message}" };
+            }
         }
 
         public IEnumerable<tbPermisos> FindPermission(tbPermisos? item)
@@ -68,8 +95,14 @@ namespace SIDCOP_Backend.DataAccess.Repositories.Acceso
 
         public RequestStatus Delete(tbPermisos? item)
         {
+            if (item == null)
+            {
+                return new RequestStatus { code_Status = 0, message_Status = "Los datos llegaron vacios o datos erroneos" };
+            }
+
             var parameter = new DynamicParameters();
             parameter.Add("@Perm_Id", item.Perm_Id, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
+
             try
             {
                 using var db = new SqlConnection(SIDCOP_Context.ConnectionString);
