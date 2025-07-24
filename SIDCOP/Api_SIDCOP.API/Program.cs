5eac91510ab0be8a2c5020f0aec123cba7876fc0
Sebastian Mejia <sebastianandremejia@gmail.com>
2025-07-24 08:33:39 -0600

    Update Program.cs

--- SIDCOP/Api_SIDCOP.API/Program.cs
+++ SIDCOP/Api_SIDCOP.API/Program.cs
@@ -73,6 +73,11 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(5000); // Escucha en todas las IP
+});
+
 builder.Services.AddSingleton<ApiKeyAuthorizationFilter>();
 builder.Services.AddSingleton<IApiKeyValidator, ApiKeyValidator>();
 
