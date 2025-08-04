387e5eec5fec7b62647a4c6806cceb537546e8da
Sebastian Mejia <sebastianandremejia@gmail.com>
2025-08-03 23:22:54 -0600

    Update tbUsuarios.cs

--- SIDCOP/SIDCOP_Backend.Entities/Entities/tbUsuarios.cs
+++ SIDCOP/SIDCOP_Backend.Entities/Entities/tbUsuarios.cs
@@ -12,7 +12,7 @@ public partial class tbUsuarios
 
     public string Usua_Usuario { get; set; }
 
-    public string Usua_Clave { get; set; }
+    public byte[] Usua_Clave { get; set; }
 
     public int Role_Id { get; set; }
 
@@ -23,6 +23,8 @@ public partial class tbUsuarios
     public bool Usua_EsAdmin { get; set; }
 
     public string Usua_Imagen { get; set; }
+
+    public bool? Usua_TienePermisos { get; set; }
 
     public int Usua_Creacion { get; set; }
 
@@ -68,6 +70,9 @@ public partial class tbUsuarios
 
     public virtual tbUsuarios Usua_ModificacionNavigation { get; set; }
 
+    public virtual ICollection<tbPromocionesPorClientes> tbPromocionesPorClientesUsua_CreacionNavigation { get; set; } = new List<tbPromocionesPorClientes>();
+
+    public virtual ICollection<tbPromocionesPorClientes> tbPromocionesPorClientesUsua_ModificacionNavigation { get; set; } = new List<tbPromocionesPorClientes>();
     public virtual ICollection<tbAvales> tbAvalesUsua_CreacionNavigation { get; set; } = new List<tbAvales>();
 
     public virtual ICollection<tbAvales> tbAvalesUsua_ModificacionNavigation { get; set; } = new List<tbAvales>();
@@ -215,10 +220,6 @@ public partial class tbUsuarios
     public virtual ICollection<tbPromocionesDetalle> tbPromocionesDetalleUsua_CreacionNavigation { get; set; } = new List<tbPromocionesDetalle>();
 
     public virtual ICollection<tbPromocionesDetalle> tbPromocionesDetalleUsua_ModificacionNavigation { get; set; } = new List<tbPromocionesDetalle>();
-
-    public virtual ICollection<tbPromocionesPorClientes> tbPromocionesPorClientesUsua_CreacionNavigation { get; set; } = new List<tbPromocionesPorClientes>();
-
-    public virtual ICollection<tbPromocionesPorClientes> tbPromocionesPorClientesUsua_ModificacionNavigation { get; set; } = new List<tbPromocionesPorClientes>();
 
     public virtual ICollection<tbProveedores> tbProveedoresUsua_CreacionNavigation { get; set; } = new List<tbProveedores>();
 
