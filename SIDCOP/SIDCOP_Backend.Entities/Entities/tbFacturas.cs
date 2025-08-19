5d497cc7f95df7f2d46cf387c8f56a223951a863
JosueMencia <mjosuedavid12@gmail.com>
2025-08-19 09:23:09 -0600

    fix Factura entities

--- SIDCOP/SIDCOP_Backend.Entities/Entities/tbFacturas.cs
+++ SIDCOP/SIDCOP_Backend.Entities/Entities/tbFacturas.cs
@@ -12,28 +12,15 @@ public partial class tbFacturas
 
     public int Fact_Id { get; set; }
 
+    public int DiCl_Id { get; set; }
+
     public string Fact_Numero { get; set; }
 
     public string Fact_TipoDeDocumento { get; set; }
 
     public int RegC_Id { get; set; }
 
-    public int Clie_Id { get; set; }
-
-    [NotMapped]
-    public string? Clie_NombreCompleto { get; set; }
-
-
-    [NotMapped]
-    public string? Clie_NombreNegocio { get; set; }
-
     public int Vend_Id { get; set; }
-
-    [NotMapped]
-    public string? Vend_NombreCompleto { get; set; }
-
-    [NotMapped]
-    public string? Motivo { get; set; }
 
     public string Fact_TipoVenta { get; set; }
 
@@ -85,7 +72,24 @@ public partial class tbFacturas
 
     public bool Fact_Estado { get; set; }
 
-    public virtual tbClientes Clie { get; set; }
+    public bool Fact_EsPedido { get; set; }
+
+    public int Pedi_Id { get; set; }
+
+    [NotMapped]
+    public string? Clie_NombreCompleto { get; set; }
+
+
+    [NotMapped]
+    public string? Clie_NombreNegocio { get; set; }
+
+    [NotMapped]
+    public string? Vend_NombreCompleto { get; set; }
+
+    [NotMapped]
+    public string? Motivo { get; set; }
+
+    public virtual tbDireccionesPorCliente DiCl { get; set; }
 
     public virtual tbRegistrosCAI RegC { get; set; }
 
@@ -101,6 +105,7 @@ public partial class tbFacturas
 
     public virtual ICollection<tbFacturasDetalle> tbFacturasDetalle { get; set; } = new List<tbFacturasDetalle>();
 }
+
 
 public class VentaInsertarDTO
 {
@@ -291,7 +296,3 @@ public class FacturaVendedorDTO
     public string Mensaje { get; set; }
     public bool Exitoso { get; set; }
 }
-
-
-
-
