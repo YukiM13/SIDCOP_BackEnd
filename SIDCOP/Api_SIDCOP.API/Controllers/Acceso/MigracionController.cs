572c1e800f9e9c7b1d4fbb42a904eda49f06d2a8
Sebastian Mejia <sebastianandremejia@gmail.com>
2025-08-19 20:35:09 -0600

    feat(migracion etl)

---
+++ SIDCOP/Api_SIDCOP.API/Controllers/Acceso/MigracionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api_SIDCOP.API.Controllers.Acceso
+{
+    public class MigracionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
