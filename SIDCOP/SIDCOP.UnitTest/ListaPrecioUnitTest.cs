46b023ea1dd6c3db514383336776413d1f6a46c6
Samuel <snmch13@gmail.com>
2025-09-23 11:28:27 -0600

    .

--- SIDCOP/SIDCOP.UnitTest/ListaPrecioUnitTest.cs
+++ SIDCOP/SIDCOP.UnitTest/ListaPrecioUnitTest.cs
@@ -66,40 +66,47 @@ namespace SIDCOP.UnitTest
 
             //el numero es según la cantidad de objetos que le hayamos puesto a la tabla de arriba dado que es el
             //"resultado esperado"
+            //esto ejecuta la funcion del repositorio
             _repository.Setup(pl => pl.ListPorProducto(3))
                 .Returns(modelo);
 
-            //lo mismo aqui del "resultado esperado"
+            //lo mismo aqui del "resultado esperado" ejecutando el service esta vez y guardando el result
             var result = _service.ListPreciosPorProducto_PorProducto(3);
 
-            //cantidad de objetos, sin mas eso
+            //cantidad de objetos esperada
             result.Should().HaveCount(3);
 
             //un registor que contenga algo en especifico y no se repita (sirve mas que nada para las pk)
             result.Should().ContainSingle(x => x.Prod_Id == 2);
         }
 
+        //ya se explico arriba
         [Fact]
+
+        //unit test para insertar lista precio
         public void ListaPrecioInsertar()
         {
             //declaramos un elemento a insertar (que lleve algo aunque sea)
             var item = new tbPreciosPorProducto { Prod_Id = 10 };
 
-            //el insertar del repositorio
+            //el insertar del repositorio con las cosas esperadas que devuelva
             _repository.Setup(pl => pl.InsertLista(item))
               .Returns(new RequestStatus { code_Status = 1, message_Status = "Precios registrados correctamente." });
             //
 
-            //ejecuta el insert del service y da el result
+            //ejecuta el insert del service y guarda el result de este mismo
             var result = _service.InsertPreciosPorProductoLista(item);
 
             //success por como lo tenemos siempre dara true así que luego evaluamos lo del data que se manda desde
             //el sp en la base de datos
             result.Success.Should().BeTrue();
+
+            //cosas del data del sp
             //si el code_Status es un entonces si se inserto, en caso que tire error es que no inserto nada
             ((int)result.Data.code_Status).Should().Be(1);
             //si el message_Status es el esperado entonces se cumplio que si inserto
             ((string)result.Data.message_Status).Should().Be("Precios registrados correctamente.");
+            //validar que se llamo solo una vez durante la ejecucion
             _repository.Verify(r => r.InsertLista(item), Times.Once);
 
         }
