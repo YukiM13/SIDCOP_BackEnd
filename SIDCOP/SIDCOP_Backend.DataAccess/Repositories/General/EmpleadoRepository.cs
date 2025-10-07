6ce877b6c72f94facdbace4de613d8ca426cce66
Sloshypenguin <alejandrogaleas.64@gmail.com>
2025-10-07 13:52:02 -0600

    feat (Empleados unit test)

--- SIDCOP/SIDCOP_Backend.DataAccess/Repositories/General/EmpleadoRepository.cs
+++ SIDCOP/SIDCOP_Backend.DataAccess/Repositories/General/EmpleadoRepository.cs
@@ -11,7 +11,7 @@ namespace SIDCOP_Backend.DataAccess.Repositories.General
 {
     public class EmpleadoRepository : IRepository<tbEmpleados>
     {
-        public RequestStatus Delete(int? id)
+        public virtual RequestStatus Delete(int? id)
         {
             var parameter = new DynamicParameters();
             parameter.Add("@Empl_Id", id, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
@@ -31,9 +31,7 @@ namespace SIDCOP_Backend.DataAccess.Repositories.General
             }
         }
 
-
-
-        public tbEmpleados Find(int? id)
+        public virtual tbEmpleados Find(int? id)
         {
             using var db = new SqlConnection(SIDCOP_Context.ConnectionString);
             var parameter = new DynamicParameters();
@@ -46,8 +44,7 @@ namespace SIDCOP_Backend.DataAccess.Repositories.General
             return result;
         }
 
-
-        public RequestStatus Insert(tbEmpleados item)
+        public virtual RequestStatus Insert(tbEmpleados item)
         {
             if (item == null)
             {
@@ -60,7 +57,7 @@ namespace SIDCOP_Backend.DataAccess.Repositories.General
             parameter.Add("@Empl_Apellidos", item.Empl_Apellidos, System.Data.DbType.String, System.Data.ParameterDirection.Input);
             parameter.Add("@Empl_Sexo", item.Empl_Sexo, System.Data.DbType.String, System.Data.ParameterDirection.Input);
             parameter.Add("@Empl_FechaNacimiento", item.Empl_FechaNacimiento, System.Data.DbType.DateTime, System.Data.ParameterDirection.Input);
-            parameter.Add("@Empl_Correo", item.Empl_Correo, System.Data.DbType.String, System.Data.ParameterDirection.Input); 
+            parameter.Add("@Empl_Correo", item.Empl_Correo, System.Data.DbType.String, System.Data.ParameterDirection.Input);
             parameter.Add("@Empl_Telefono", item.Empl_Telefono, System.Data.DbType.String, System.Data.ParameterDirection.Input);
             parameter.Add("@Sucu_Id", item.Sucu_Id, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
             parameter.Add("@EsCv_Id", item.EsCv_Id, System.Data.DbType.Int32, System.Data.ParameterDirection.Input);
@@ -85,19 +82,16 @@ namespace SIDCOP_Backend.DataAccess.Repositories.General
             {
                 return new RequestStatus { code_Status = 0, message_Status = $"Error inesperado: {ex.Message}" };
             }
-
         }
 
-
-
-        public IEnumerable<tbEmpleados> List()
+        public virtual IEnumerable<tbEmpleados> List()
         {
             using var db = new SqlConnection(SIDCOP_Context.ConnectionString);
             var result = db.Query<tbEmpleados>(ScriptDatabase.Empleados_Listar, commandType: System.Data.CommandType.StoredProcedure);
             return result;
         }
 
-        public RequestStatus Update(tbEmpleados item)
+        public virtual RequestStatus Update(tbEmpleados item)
         {
             if (item == null)
             {
@@ -136,7 +130,5 @@ namespace SIDCOP_Backend.DataAccess.Repositories.General
                 return new RequestStatus { code_Status = 0, message_Status = $"Error inesperado: {ex.Message}" };
             }
         }
-
-
     }
 }
