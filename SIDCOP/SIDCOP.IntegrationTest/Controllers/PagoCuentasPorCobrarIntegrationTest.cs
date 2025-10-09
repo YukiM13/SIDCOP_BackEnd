eb3f9fc44dc6650a542f8746dca4a5383bac68d6
SOLANGRI69 <ajsl69777@gmail.com>
2025-10-09 11:31:06 -0600

    09/10/2025 Solo Falta Arreglar El Insertar Pago

---
+++ SIDCOP/SIDCOP.IntegrationTest/Controllers/PagoCuentasPorCobrarIntegrationTest.cs
@@ -0,0 +1,79 @@
+﻿using Org.BouncyCastle.Asn1;
+using SIDCOP.IntegrationTest.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIDCOP.IntegrationTest.Controllers
+{
+    [TestClass]
+    public class PagoCuentasPorCobrarIntegrationTest : BaseIntegrationTest
+    {
+        private const string ApiKey = "bdccf3f3-d486-4e1e-ab44-74081aefcdbc";
+
+        [TestMethod]
+        public async Task PagosCuentasPorCobrar_Insertar()
+        {
+            var cliente = factory.CreateClient();
+            cliente.DefaultRequestHeaders.Add("X-API-Key", ApiKey);
+
+            var pagoMock = PagoCuentaPorCobrarMocks.CrearMockPagoCuentaPorCobrarInsertar();
+
+            var contenido = new StringContent(
+                System.Text.Json.JsonSerializer.Serialize(pagoMock),
+                System.Text.Encoding.UTF8,
+                "application/json"
+            );
+
+            var response = await cliente.PostAsync("/PagosCuentasPorCobrar/Insertar", contenido);
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
+            Assert.IsNotNull(response);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.IsFalse(string.IsNullOrEmpty(responseContent));
+            Console.WriteLine($"Respuesta del servidor: {responseContent}");
+        }
+
+        [TestMethod]
+        public async Task PagosCuentasPorCobrar_ListarPorCuentaPorCobrar()
+        {
+            var cliente = factory.CreateClient();
+            cliente.DefaultRequestHeaders.Add("X-API-Key", ApiKey);
+            var pagoMock = PagoCuentaPorCobrarMocks.CrearMockPagoCuentaPorCobrarListarCuentaPorCobrar();
+            var contenido = new StringContent(
+                System.Text.Json.JsonSerializer.Serialize(pagoMock),
+                System.Text.Encoding.UTF8,
+                "application/json"
+            );
+            var response = await cliente.GetAsync($"/PagosCuentasPorCobrar/ListarPorCuentaPorCobrar/{pagoMock.CPCo_Id}");
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
+            Assert.IsNotNull(response);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.IsFalse(string.IsNullOrEmpty(responseContent));
+            Console.WriteLine($"Respuesta del servidor: {responseContent}");
+        }
+
+        [TestMethod]
+        public async Task PagosCuentasPorCobrar_Anular()
+        {
+            var cliente = factory.CreateClient();
+            cliente.DefaultRequestHeaders.Add("X-API-Key", ApiKey);
+
+            var pagoMock = PagoCuentaPorCobrarMocks.CrearMockPagoCuentaPorCobrarAnular();
+
+            var contenido = new StringContent(
+                System.Text.Json.JsonSerializer.Serialize(pagoMock),
+                System.Text.Encoding.UTF8,
+                "application/json"
+            );
+
+            var response = await cliente.PostAsync("/PagosCuentasPorCobrar/Anular", contenido);
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
+            Assert.IsNotNull(response);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.IsFalse(string.IsNullOrEmpty(responseContent));
+            Console.WriteLine($"Respuesta del servidor: {responseContent}");
+        }
+    }
+}
---
+++ SIDCOP/SIDCOP.IntegrationTest/Mocks/PagoCuentaPorCobrarMocks.cs
@@ -0,0 +1,43 @@
+﻿using Api_SIDCOP.API.Models.Ventas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIDCOP.IntegrationTest.Mocks
+{
+    public class PagoCuentaPorCobrarMocks
+    {
+        public static PagosCuentasPorCobrarViewModel CrearMockPagoCuentaPorCobrarInsertar()
+        {
+            return new PagosCuentasPorCobrarViewModel
+            {
+                CPCo_Id = 81,
+                Pago_Monto = 69,
+                FoPa_Id = 1,
+                Pago_NumeroReferencia = "123456",
+                Pago_Observaciones = "Pago de prueba 2.0",
+                Usua_Creacion = 1
+            };
+        }
+
+        public static PagosCuentasPorCobrarViewModel CrearMockPagoCuentaPorCobrarListarCuentaPorCobrar()
+        {
+            return new PagosCuentasPorCobrarViewModel
+            {
+                CPCo_Id = 76
+            };
+        }
+
+        public static AnularPagoViewModel CrearMockPagoCuentaPorCobrarAnular()
+        {
+            return new AnularPagoViewModel
+            {
+                Pago_Id = 2207,
+                Usua_Modificacion = 1,
+                Motivo_Anulacion = "Porque si"
+            };
+        }
+    }
+}
