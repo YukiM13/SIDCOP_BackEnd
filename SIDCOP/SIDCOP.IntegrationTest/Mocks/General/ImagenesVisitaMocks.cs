feb1d61224c7bd3d164fabfc1fce345aa78c71d0
Helen Euceda <neuceda13@gmail.com>
2025-10-10 03:03:12 -0600

    Todas las pruebas de integración de usuarios y empezando a configurar las de ImagenesVisita

---
+++ SIDCOP/SIDCOP.IntegrationTest/Mocks/General/ImagenesVisitaMocks.cs
@@ -0,0 +1,25 @@
+﻿using Api_SIDCOP.API.Models.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIDCOP.IntegrationTest.Mocks.General
+{
+    public class ImagenesVisitaMocks
+    {
+        public static ImagenVisitaViewModel     ListarUsuariosMock()
+        {
+            return new ImagenVisitaViewModel
+            {
+                ImVi_Id = 74,
+                ImVi_Imagen = "heuceda",
+                ClVi_Id = 3,
+                Usua_Creacion = 35,
+                ImVi_FechaCreacion = DateTime.Now,
+
+            };
+        }
+    }
+}
