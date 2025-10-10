21abab9c9640eb083f6ac1f435edc70e86df2f5a
AlexxxMikuUwU <xdavid927@gmail.com>
2025-10-10 11:19:03 -0600

    feat(Cliente Listar Prueba Integracion)

---
+++ SIDCOP/SIDCOP.IntegrationTest/Controllers/General/ClientesIntegrationTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIDCOP.IntegrationTest.Controllers.General
+{
+    [TestClass]
+    public class ClientesIntegrationTest: BaseIntegrationTest
+    {
+        private const string ApiKey = "bdccf3f3-d486-4e1e-ab44-74081aefcdbc";
+
+        [TestMethod]
+        public async Task Clientes_Listar()
+        {
+            var cliente = factory.CreateClient();
+            cliente.DefaultRequestHeaders.Add("X-API-Key", ApiKey);
+            var response = await cliente.GetAsync("/Cliente/Listar");
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
+            Assert.IsNotNull(response);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.IsFalse(string.IsNullOrEmpty(responseContent));
+            Console.WriteLine($"Respuesta del servidor: {responseContent}");
+        }
+    }
+}
---
+++ SIDCOP/SIDCOP.IntegrationTest/Mocks/General/ClientesMocks.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Api_SIDCOP.API.Models.General;
+
+namespace SIDCOP.IntegrationTest.Mocks.General
+{
+    public class ClientesMocks
+    {
+    }
+}
